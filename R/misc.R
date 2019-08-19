ddb268edd733baf49f4b446dae8a3f7c3bef83cb
Alex Chubaty <alex.chubaty@gmail.com>
2019-08-19 12:43:38 -0600

    add helper function 'simFile()'
    assists with filename creation for simulation outputs

---
+++ R/misc.R
@@ -0,0 +1,18 @@
+#' Generate simulation file name
+#'
+#' Assists with saving/retrieving LandWeb simulations.
+#'
+#' @param name Object name (e.g., \code{"mySimOut"})
+#' @param path Directory location in where the file will be located (e.g., an \code{outputPath}).
+#' @param time Optional simulation time to use as filename suffix.
+#'
+#' @export
+#' @importFrom SpaDES.core paddedFloatToChar
+#' @importFrom reproducible normPath
+simFile <- function(name, path, time) {
+  if (is.null(time))
+    file.path(normPath(path), paste0(name, ".rds"))
+  else {
+    file.path(normPath(path), paste0(name, "_", paddedFloatToChar(time, padL = 4), ".rds"))
+  }
+}
