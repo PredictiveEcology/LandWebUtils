bbf6f3cc69b22196db82f1456fa78cad6ba665c1
Alex Chubaty <alex.chubaty@gmail.com>
2020-05-07 12:23:49 -0600

    remove LandWeb-specific species param values
    @CeresBarros i will move `updateSpeciesTable()` to `LandR` next

--- R/speciesTable.R
+++ R/speciesTable.R
@@ -1,18 +1,11 @@
-if (getRversion() >= "3.1.0") {
-  utils::globalVariables(c("growthcurve", "longevity", "mortalityshape", "postfireregen",
-                           "resproutage_max", "resproutage_min", "resproutprob",
-                           "shadetolerance", "species"))
-}
-
-#' Customize species trait table values for LandWeb
+#' Customize species trait table values
 #'
 #' @param speciesTable The species traits table
-#' @param runName      Optional. Character string indicating the LandWeb run name (scenario).
 #' @param params       A named list (of parameters) of named lists (by species), with species
 #'                     traits overrides (e.g., \code{list(seeddistance_eff = list(Abie_sp = 25))}).
 #'
 #' @export
-updateSpeciesTable <- function(speciesTable, runName = NULL, params) {
+updateSpeciesTable <- function(speciesTable, params) {
   ## checks:
   traits <- names(params)
   missingTraits <- traits[!traits %in% names(speciesTable)]
@@ -44,53 +37,6 @@ updateSpeciesTable <- function(speciesTable, runName = NULL, params) {
     for (sp in spp) {
       set(speciesTable, which(speciesTable$species == sp), trt, subParams[[sp]])
     }
-  }
-
-  if (!is.null(runName)) {
-    if (grepl("noDispersal|aspenDispersal|highDispersal", runName)) {
-      speciesTable[, postfireregen := "resprout"] ## force all species to resprout
-      speciesTable[, resproutprob := 1.0]     # default 0.5
-      speciesTable[, resproutage_min := 0]    # defaults vary by species
-      speciesTable[, resproutage_max := 400]  # defaults vary by species
-
-      # decrease shade tolerance
-      speciesTable[species == "Abie_sp", shadetolerance := 3]     # original default 4
-      speciesTable[species == "Pice_gla", shadetolerance := 2]    # original default 3
-      speciesTable[species == "Pice_mar", shadetolerance := 3]    # original default 4
-      speciesTable[species == "Pinu_ban", shadetolerance := 1]    # original default 1
-      speciesTable[species == "Pinu_con", shadetolerance := 1]    # original default 1
-      speciesTable[species == "Pinu_sp", shadetolerance := 1]     # original default 1
-      speciesTable[species == "Popu_sp", shadetolerance := 1]     # original default 1
-    }
-
-    if (grepl("aspen80", runName)) {
-      speciesTable[species == "Popu_sp", longevity := 80] # default 150
-    }
-
-    ## TODO: THESE SHOULD BE PULLED OUT OF THE FUNCTION AND PASSED VIA
-    ## THE params ARGUMENT.
-    ## growth curves:
-    #   Biomass Succession User Guide p17, 0 is faster growth, 1 was the prev assumption
-    speciesTable[species == "Abie_sp", growthcurve := 0]  # original default 0
-    speciesTable[species == "Pice_gla", growthcurve := 1] # original default 1
-    speciesTable[species == "Pice_mar", growthcurve := 1] # original default 1
-    speciesTable[species == "Pinu_ban", growthcurve := 0] # original default 0
-    speciesTable[species == "Pinu_con", growthcurve := 0] # original default 0
-    speciesTable[species == "Pinu_sp", growthcurve := 0]  # original default 0
-    speciesTable[species == "Popu_sp", growthcurve := 0]  # original default 0
-
-    ## mortality
-    speciesTable[species == "Abie_sp", mortalityshape := 15]  # default 15
-    speciesTable[species == "Pice_gla", mortalityshape := 15] # default 15
-    speciesTable[species == "Pice_mar", mortalityshape := 15] # default 15
-    speciesTable[species == "Pinu_ban", mortalityshape := 15] # default 15
-    speciesTable[species == "Pinu_con", mortalityshape := 15] # default 15
-    speciesTable[species == "Pinu_sp", mortalityshape := 15]  # default 15
-    speciesTable[species == "Popu_sp", mortalityshape := 25]  # default 25
-
-    ## resprouting (normally, only aspen resprouts)
-    speciesTable[species == "Popu_sp", resproutage_min := 25] # default 10
-    #speciesTable[species == "Popu_sp", resproutprob := 0.1]  # default 0.5
   }
 
   return(speciesTable)
