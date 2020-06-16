44d0872e07c8556dc3e2466a6319c58df019de5b
Alex Chubaty <alex.chubaty@gmail.com>
2020-06-16 10:09:21 -0600

    add 'cleanAreaName()'

--- R/misc.R
+++ R/misc.R
@@ -4,6 +4,17 @@
 #' @keywords internal
 .ageClasses <- c("Young", "Immature", "Mature", "Old")
 
+#' Extract study area name from run name
+#'
+#' @param area Simulated area (i.e., run) name
+#'
+#' @export
+cleanAreaName <- Vectorize(function(area) {
+  strsplit(area, "_")[[1]] %>%
+    grep("Dispersal|ROS", ., invert = TRUE, value = TRUE) %>%
+    paste(., collapse = "_")
+})
+
 #' Generate simulation file name
 #'
 #' Assists with saving/retrieving LandWeb simulations.
