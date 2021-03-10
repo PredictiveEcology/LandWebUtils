c8caaa3caa0e3697a763014b27a8ae7555a9f289
Alex Chubaty <achubaty@for-cast.ca>
2021-03-10 11:11:30 -0700

    move 'simFile()' to SpaDES.core

--- R/misc.R
+++ R/misc.R
@@ -14,22 +14,3 @@ cleanAreaName <- Vectorize(function(area) {
     grep("Dispersal|ROS", ., invert = TRUE, value = TRUE) %>%
     paste(., collapse = "_")
 })
-
-#' Generate simulation file name
-#'
-#' Assists with saving/retrieving LandWeb simulations.
-#'
-#' @param name Object name (e.g., \code{"mySimOut"})
-#' @param path Directory location in where the file will be located (e.g., an \code{outputPath}).
-#' @param time Optional simulation time to use as filename suffix. Default \code{NULL}.
-#' @param ext  The file extension to use (default \code{"rds"}).
-#' @export
-#' @importFrom SpaDES.core paddedFloatToChar
-#' @importFrom reproducible normPath
-simFile <- function(name, path, time = NULL, ext = "rds") {
-  if (is.null(time))
-    file.path(normPath(path), paste0(name, ".", ext))
-  else {
-    file.path(normPath(path), paste0(name, "_", paddedFloatToChar(time, padL = 4), ".", ext))
-  }
-}
