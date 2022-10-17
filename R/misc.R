38dfebf4319ad3baf2097f0ef62d5903582f4c73
Alex Chubaty <achubaty@for-cast.ca>
2022-10-17 10:31:35 -0600

    analysesOutputsTimes as function
    use standard definition for use in mutiple places

--- R/misc.R
+++ R/misc.R
@@ -4,6 +4,19 @@
 #' @keywords internal
 .ageClasses <- c("Young", "Immature", "Mature", "Old")
 
+#' Simulation timesteps for analyses
+#'
+#' @param period numeric vector of length 2 corresponding to the start and end times
+#'               to use for analyses.
+#'
+#' @param interval numeric indicating the interval between timesteps for analyses
+#'
+#' @export
+#' @return numeric vector of timesteps for which to run analyses
+analysesOutputsTimes <- function(period, interval) {
+  seq(period[1], period[2], by = interval)
+}
+
 #' Extract study area name from run name
 #'
 #' @param area Simulated area (i.e., run) name
